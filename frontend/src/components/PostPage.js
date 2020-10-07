df31ec2d06491af41eb28d378329684d9d9a3a31
StedHouston <s.houston406@gmail.com>
2020-10-07 12:13:15 -0500

    post/comments page work

--- frontend/src/components/PostPage.js
+++ frontend/src/components/PostPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import SubredditHeader from './SubredditHeader';
+import AboutCommunity from './AboutCommunity';
+import './PostPage.css';
+
 
 
 
@@ -12,10 +14,19 @@ function PostPage(){
 
     return (
         <>
-            <SubredditHeader/>
             <div className="PostPage">
-                <div className="PostPage__PostandComments">Post Page</div>
-                <div className="PostPage__About">About</div>
+                <div className="PostPage__Container">
+                    <div className="PostPage__PostandComments">
+                        <div className="PostPage__Post">
+                            <div>r/Bitcoin Posted by u/John Doe</div>
+                            <div>Frustrated: Litecoin lacking trustworthy Ambassadors and Social Influencers, ideas?</div>
+                            <div>As a long time Litecoin supporter, I've been very disappointed in some of the ambassadors responsible for advancing and sharing the value that it provides. I understand - this is a decentralized currency and we all share in the effort to increase awareness and adoption (nor can we control who supports it). However, the reality is that much of the most high profile community members, the folks that pop up when you go to learn about Litecoin, are irresponsible conspiracists at best, con artists at worst.
+                                Everything from Qanon insanity, to bible based time studies, remote "psychic" viewing, to doomsday preppers selling food. Honestly - many of the videos and posts are just disgusting! When many of us started this journey we believed in Charlie and his path toward creating sound digital money. If I was a new user coming into the space and saw the amount of nonsense attached to Litecoin I would run for the hills. If these are the people Litecoin attracts, do you really see everyday Americans and businesses gaining trust?
+                                What can we do to inspire more responsible ambassadors and messaging? How else can the community inspire new adoption? What's working with other coins? This post isn't meant to be a complaint, it's to come up with a list of ways to infuse the community with new life and ideally more responsible messaging. By the way - love the Litecoin Foundation partnerships - but they can't do everything. How can WE do more?</div>
+                        </div>
+                    </div>
+                    <div className="PostPage__About">About</div>
+                </div>
             </div>
         </>
     )
